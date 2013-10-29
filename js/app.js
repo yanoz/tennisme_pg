91220bdb0d849ed26b49d38a98d44c45d713eaad
yanoz <yann.perchec@novapost.fr>
2013-10-29 13:04:36 +0100

    menu

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
-var app = angular.module('tennis-me', ['']);
-
+angular.module('super-awesome-demo',['shoppinpal.mobile-menu'])
+    .config(['$routeProvider',function($routeProvider){
+        $routeProvider
+            .when("/skinny", {
+                templateUrl: "skinny.html"//,
+                //controller: "SkinnyCtrl"
+            })
+            .when("/wide", {
+                templateUrl: "wide.html"//,
+                //controller: "WideCtrl"
+            })
+            .otherwise({
+                redirectTo: "/skinny"
+            });
+    }]);
+//    .controller('SkinnyCtrl', ['$scope', '$routeParams', DummyCtrl])
+//    .controller('WideCtrl', ['$scope', '$routeParams', DummyCtrl]);
+//
+//function DummyCtrl($scope, $routeParams) {
+//
+//}
