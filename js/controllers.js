9ef02cd63b37f7e87531b7402352cc763e9c307e
yanoz <yann.perchec@novapost.fr>
2013-11-25 13:54:14 +0100

    just some code for levels index

--- js/controllers.js
+++ js/controllers.js
@@ -23,15 +23,50 @@ function ClubsCtrl ($scope, $http) {
 };
 
 function ProfileCtrl ($scope, $http, getProfile) {
+ $scope.levelsMap={
+    '0': 'nc',
+    '1': '30/5',
+    '2': '30/4', 
+    '3': '30/3',
+    '4': '30/2',
+    '5': '30/1',
+    '6': '30',
+    '7': '15/5',
+    '8': '15/4',
+    '9': '15/3',
+    '10': '15/2',
+    '11': '15/1',
+    '12': '15',
+    '13': '5/6',
+    '14': '4/6',
+    '15': '3/6',
+    '16': '2/6',
+    '17': '1/6',
+    '18': '0',
+    '19': '-2/6',
+    '20': '-4/6',
+    '21': '-15',
+    '22': '-30',
+    '23': 'Prom',
+    '24': '1ère',
+  }
+  Object.prototype.getKeyByValue = function( value ) {
+    for( var prop in this ) {
+        if( this.hasOwnProperty( prop ) ) {
+             if( this[ prop ] === value )
+                 return prop;
+        }
+    }
+  }
   getProfile.async().then(function(d) {
     $scope.name = d.fullname;
     $scope.email = d.email;
     $scope.gender = d.gender;
-    $scope.level = $scope.levels[d.level];
+    $scope.level = $scope.levelsMap[d.level];
     $scope.dob = d.date_of_birth;
   });
   $scope.levelSelected = undefined;
-  $scope.levels=["nc", "30/5","30/4","30/3","30/2","30/1","30","15/5","15/4","15/3","15/2","15/1","15","5/6","4/6","3/6","2/6","1/6","0","-2/6","-4/6","-15","-30","Prom","1ère"];
+  // $scope.value = levelsMap.getKeyByValue($scope.level);
 
   $scope.validate = function()  {
     $http.defaults.useXDomain = true;
